809e86b2c8053fcdcd8f42fc6422e926a931dbb5
nicolassampaiorj-tech <nicolassampaiorj@gmail.com>
2025-11-25 10:16:53 +0000

    added some codes

--- src/myfirstgit/MyFirstGit.java
+++ src/myfirstgit/MyFirstGit.java
@@ -16,7 +16,9 @@ public class MyFirstGit {
     public static void main(String[] args) {
         // TODO code application logic here
 
-
+        System.out.println("Hello folks");
+        
+        System.out.println("blabla");
 
 
 
